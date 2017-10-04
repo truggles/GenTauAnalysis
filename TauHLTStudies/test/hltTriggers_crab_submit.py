9c191173b3a7a7750f8e71a22c3f8cdb61904526
Tyler Ruggles <truggles@wisc.edu>
2017-10-04 13:29:32 +0200

    Adding DYJets, ordered dict to avoid LumiMask problem

--- TauHLTStudies/test/hltTriggers_crab_submit.py
+++ TauHLTStudies/test/hltTriggers_crab_submit.py
@@ -20,17 +20,19 @@ config.Site.storageSite        = 'T2_US_Wisconsin'
 
 config.User.voGroup            = 'uscms'
 
-dataMap = {
-    'dataSingleMuonBv1' : '/SingleMuon/Run2017B-PromptReco-v1/MINIAOD',
-    'dataSingleMuonBv2' : '/SingleMuon/Run2017B-PromptReco-v2/MINIAOD',
-    'dataSingleMuonCv1' : '/SingleMuon/Run2017C-PromptReco-v1/MINIAOD',
-    'dataSingleMuonCv2' : '/SingleMuon/Run2017C-PromptReco-v2/MINIAOD',
-    'dataSingleMuonCv3' : '/SingleMuon/Run2017C-PromptReco-v3/MINIAOD',
-    'dataSingleMuonDv1' : '/SingleMuon/Run2017D-PromptReco-v1/MINIAOD',
-    'dataSingleMuonEv1' : '/SingleMuon/Run2017E-PromptReco-v1/MINIAOD',
-    'ggH125' : '/GluGluHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM',
-    'qqH125' : '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM',
-}
+dataMap = OrderedDict()
+#dataMap['ggH125'] = '/GluGluHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM'
+#dataMap['qqH125'] = '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM'
+dataMap['DYJets'] = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v7-v1/MINIAODSIM'
+dataMap['DYJetsExt1'] = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10_ext1-v2/MINIAODSIM'
+dataMap['DYJetsExt2'] = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10_ext1-v1/MINIAODSIM'
+#dataMap['dataSingleMuonBv1'] = '/SingleMuon/Run2017B-PromptReco-v1/MINIAOD'
+#dataMap['dataSingleMuonBv2'] = '/SingleMuon/Run2017B-PromptReco-v2/MINIAOD'
+#dataMap['dataSingleMuonCv1'] = '/SingleMuon/Run2017C-PromptReco-v1/MINIAOD'
+#dataMap['dataSingleMuonCv2'] = '/SingleMuon/Run2017C-PromptReco-v2/MINIAOD'
+#dataMap['dataSingleMuonCv3'] = '/SingleMuon/Run2017C-PromptReco-v3/MINIAOD'
+#dataMap['dataSingleMuonDv1'] = '/SingleMuon/Run2017D-PromptReco-v1/MINIAOD'
+#dataMap['dataSingleMuonEv1'] = '/SingleMuon/Run2017E-PromptReco-v1/MINIAOD'
 
 if __name__ == '__main__':
 

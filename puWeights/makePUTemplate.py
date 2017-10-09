4f85109df2067826601eb494d05041bfad600102
Tyler Ruggles <truggles@wisc.edu>
2017-10-09 14:27:22 +0200

    Make 2017 PU template

---
+++ puWeights/makePUTemplate.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+
+# New minBias xsec: https://hypernews.cern.ch/HyperNews/CMS/get/luminosity/613/2/1/1/1.html
+def makeDataPUTemplate( cert, puJson, year='17' ) :
+    executeArray = [
+        'pileupCalc.py',
+        '-i',
+        '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions%s/13TeV/%s' % (year, cert),
+        '--inputLumiJSON',
+        '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions%s/13TeV/PileUp/%s' % (year, puJson),
+        '--calcMode',
+        'true',
+        '--minBiasXsec',
+        '69200',
+        '--maxPileupBin',
+        '80',
+        '--numPileupBins',
+        '800',
+        'DataTemplate.root']
+    subprocess.call( executeArray )
+
+
+
+if __name__ == '__main__' :
+
+    cert = 'PromptReco/Cert_294927-303825_13TeV_PromptReco_Collisions17_JSON.txt' # 18.90/fb
+    year = '17'
+    makeDataPUTemplate( cert, 'pileup_latest.txt', year )
+
+

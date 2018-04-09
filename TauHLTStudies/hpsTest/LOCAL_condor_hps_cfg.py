aceedb4be8fdf6f970ede5bd241f1663f4869f64
Tyler Ruggles <truggles@wisc.edu>
2018-04-09 22:27:05 +0200

    Example of how to run locally

---
+++ TauHLTStudies/hpsTest/LOCAL_condor_hps_cfg.py
@@ -0,0 +1,110 @@
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("LOCALHPS")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        #'file:/hdfs/store/user/truggles/VBFHToTauTau_M125_13TeV_powheg_pythia8/qqH125_hps_1003_april09_test_v1/180409_072416/0000/output_8.root'
+        #'file:/hdfs/store/user/truggles/VBFHToTauTau_M125_13TeV_powheg_pythia8/qqH125_hps_1003_april09_test_v2/180409_152132/0000/output_9.root'
+        'file:root://cmsxrootd.fnal.gov//store/user/truggles/VBFHToTauTau_M125_13TeV_powheg_pythia8/qqH125_hps_1003_april09_test_v2/180409_152132/0000/output_9.root'
+    )
+)
+
+
+# ---- Global Tag :
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, '92X_upgrade2017_realistic_v10')
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mc2017_realistic_TSG_2017_12_19_13_49_40')
+
+#
+# Set up electron ID (VID framework)
+#
+
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+# turn on VID producer, indicate data format  to be
+# DataFormat.AOD or DataFormat.MiniAOD, as appropriate 
+dataFormat = DataFormat.MiniAOD
+
+switchOnVIDElectronIdProducer(process, dataFormat)
+
+# define which IDs we want to produce
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_GeneralPurpose_V1_cff',
+        'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_HZZ_V1_cff']
+
+#add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+
+
+
+### Gen Taus ###
+process.tauGenJets = cms.EDProducer(
+    "TauGenJetProducer",
+    GenParticles =  cms.InputTag('prunedGenParticles'),
+    includeNeutrinos = cms.bool( False ),
+    verbose = cms.untracked.bool( False )
+    )
+
+
+
+process.tauGenJetsSelectorAllHadrons = cms.EDFilter("TauGenJetDecayModeSelector",
+     src = cms.InputTag("tauGenJets"),
+     select = cms.vstring('oneProng0Pi0', 
+                          'oneProng1Pi0', 
+                          'oneProng2Pi0', 
+                          'oneProngOther',
+                          'threeProng0Pi0', 
+                          'threeProng1Pi0', 
+                          'threeProngOther', 
+                          'rare'),
+     filter = cms.bool(False)
+)
+
+
+
+process.tauGenJetsSelectorElectrons = cms.EDFilter("TauGenJetDecayModeSelector",
+     src = cms.InputTag("tauGenJets"),
+     select = cms.vstring('electron'), 
+     filter = cms.bool(False)
+)
+
+
+
+process.tauGenJetsSelectorMuons = cms.EDFilter("TauGenJetDecayModeSelector",
+     src = cms.InputTag("tauGenJets"),
+     select = cms.vstring('muon'), 
+     filter = cms.bool(False)
+)
+
+
+
+
+process.load("THRAnalysis.TauHLTStudies.hps_CfiFile_cfi")
+process.hpsTauHLTStudies.isData = cms.untracked.bool(False)
+#process.hpsTauHLTStudies.verbose = cms.untracked.bool(True)
+#process.hpsTauHLTStudies.requireMediumTauMVA = cms.untracked.bool(True)
+
+
+process.TFileService = cms.Service("TFileService",
+        fileName = cms.string("outputFileName.root")
+    )
+
+
+process.p = cms.Path(
+            #process.hltFilter*
+            process.egmGsfElectronIDSequence*
+            process.tauGenJets*
+            process.tauGenJetsSelectorAllHadrons*
+            process.tauGenJetsSelectorElectrons*
+            process.tauGenJetsSelectorMuons*
+            process.hpsTauHLTStudies)
+
+#print process.dumpPython()
+
